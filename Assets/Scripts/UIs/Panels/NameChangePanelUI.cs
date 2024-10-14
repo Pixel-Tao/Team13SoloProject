6f20dda22d0589775e700ea9d4f387e7850572a0
Tao <62868403+Pixel-Tao@users.noreply.github.com>
2024-10-14 09:33:35 +0900

    [Update] 캐릭터 이름 글자 제한

--- Assets/Scripts/UIs/Panels/NameChangePanelUI.cs
+++ Assets/Scripts/UIs/Panels/NameChangePanelUI.cs
@@ -11,15 +11,29 @@ public class NameChangePanelUI : UIBase
     [SerializeField] private TMP_InputField characterNameInputText;
     [SerializeField] private Button cancelButton;
     [SerializeField] private Button okButton;
+    [SerializeField] private TextMeshProUGUI messageText;
 
     protected override void Awake()
     {
         base.Awake();
     }
 
+    private void Start()
+    {
+        messageText.text = String.Empty;
+    }
+
     public void OnOkClick()
     {
         // 캐릭터 이름 변경 완료 클릭
+        string name = characterNameInputText.text;
+        if (string.IsNullOrEmpty(name) || name.Length < 2 || name.Length > 10)
+        {
+            // 이름이 2자 이상 10자 이하가 아니면
+            messageText.text = "이름은 2자 이상 10자 이하로 입력해주세요.";
+            return;
+        }
+
         OnOkClickEvent?.Invoke(characterNameInputText.text);
     }
 
